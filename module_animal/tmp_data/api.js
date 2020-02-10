c0bc1ac18e57765818062a6920ef937f505846f9
Piotr Rydwański <pioryd123@gmail.com>
2020-02-10 23:06:13 +0100

    Add first api structure

---
+++ module_animal/tmp_data/api.js
@@ -0,0 +1,9 @@
+module.exports = {
+  character: {
+    change_land: () => {},
+    leave_virtual_world: () => {},
+    use_object: () => {}
+  },
+  system: { form_run: () => {} },
+  virtual_world: { make_choice: () => {} }
+};
